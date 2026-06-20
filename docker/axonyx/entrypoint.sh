2359276e1dea362bea7cb0a03b622c7742a0d243
Vladan Profirovic <vladan.profirovic@gmail.com>
2026-06-21 00:31:02 +0200

    Update Docker image release defaults

--- docker/axonyx/entrypoint.sh
+++ docker/axonyx/entrypoint.sh
@@ -9,8 +9,8 @@ Axonyx demo is running.
 
 Open: http://localhost:${PORT}
 Template: docs
-CLI: cargo-axonyx ${CARGO_AXONYX_VERSION:-0.1.64}
-Runtime: axonyx-runtime ${AXONYX_RUNTIME_VERSION:-0.1.30}
+CLI: cargo-axonyx ${CARGO_AXONYX_VERSION:-0.1.72}
+Runtime: axonyx-runtime ${AXONYX_RUNTIME_VERSION:-0.1.38}
 
 EOF
 
