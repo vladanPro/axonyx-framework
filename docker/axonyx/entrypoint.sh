5e3db764c95c2f4e6b62b624b782ee296f30b6a1
Vladan Profirovic <vladan.profirovic@gmail.com>
2026-08-13 11:18:59 +0200

    Release Axonyx tooling 0.2.2

--- docker/axonyx/entrypoint.sh
+++ docker/axonyx/entrypoint.sh
@@ -9,8 +9,8 @@ Axonyx demo is running.
 
 Open: http://localhost:${PORT}
 Template: docs
-CLI: cargo-axonyx ${CARGO_AXONYX_VERSION:-0.1.87}
-Runtime: axonyx-runtime ${AXONYX_RUNTIME_VERSION:-0.1.48}
+CLI: cargo-axonyx ${CARGO_AXONYX_VERSION:-0.2.2}
+Runtime: axonyx-runtime ${AXONYX_RUNTIME_VERSION:-0.1.53}
 
 EOF
 
